6dabdddbed6cb84cfde99ebb50f64740a170a1f2
Brandon Woodson <brandon.woodson@gmail.com>
2017-02-19 15:35:18 +0000

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3030;
 
 app.use(bodyParser.json());
 
@@ -50,8 +51,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3030, () => {
-  console.log('Started on port 3030');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
