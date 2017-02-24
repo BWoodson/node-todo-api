ab93c28621259164daf5a1504be5e47c33d461dc
Brandon Woodson <brandon.woodson@gmail.com>
2017-02-24 03:00:50 +0000

    added a little salt and hash to passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 17
-};
+var password = 'abc123!';
 
-var token = jwt.sign(data, '123abc');
-console.log('token:', token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+var hashedPassword = '$2a$10$RglaaUjzyCI5ARiWYBqriuVCxcskcarOu7DJZmpFQksezEfF5MDwC';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+//var data = {
+//  id: 17
+//};
+//
+//var token = jwt.sign(data, '123abc');
+//console.log('token:', token);
+//
+//var decoded = jwt.verify(token, '123abc');
+//console.log('decoded:', decoded);
 
 //var message = 'I need more bacon!';
 //var hash = SHA256(message);
